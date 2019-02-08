9169acb9240169f4a9ff5b6faa82df953a0209b3
Chedak <cedomir.miskov@gmail.com>
2019-02-08 16:53:02 +0100

    Continue implementing new logic

--- game.rb
+++ game.rb
@@ -22,7 +22,12 @@ class Board
     @lines = {
       top:    @cells[0..2],
       middle: @cells[3..5],
-      bottom: @cells[6..8]
+      bottom: @cells[6..8],
+      left:   [@cells[0],@cells[3],@cells[6]],
+      center: [@cells[1],@cells[4],@cells[7]],
+      right:  [@cells[2],@cells[5],@cells[8]],
+      north_east: [@cells[0],@cells[4],@cells[8]],
+      north_west: [@cells[2],@cells[4],@cells[6]],
     }
   end
 
@@ -34,18 +39,21 @@ class Board
   end
 
   def check_state
-
+    x_won = @lines.any? do |line|
+              line.each {|cell| cell.state == 'x'}
+            end
+    # puts x_won
   end
 end
 
 board = Board.new
 loop do
   board.output
-  
+
   puts "Player 1, choose a number of an empty cell"
   input_p1 = gets.chomp.to_i
   board.cells[input_p1 - 1].state = "x"
-  # puts board.lines[:top][0].state
+  puts board.lines[:left][0].state
   # board.check_state
 
   # board.output
