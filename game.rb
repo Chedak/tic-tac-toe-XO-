3e244341466e77cad29209ed2ef414ec0728a0ea
Alex <nikitin2009@gmail.com>
2019-02-08 17:37:41 +0200

    Started implementing another logic

--- game.rb
+++ game.rb
@@ -1,29 +1,16 @@
 # Classes
 class Cell
-  attr_accessor :id, :row, :column, :diagonal, :state
+  attr_accessor :id, :state
 
   def initialize(id)
     @id = id
     @state = id.to_s
-
-    @row = 'top'    if id == 1 || id == 2 || id == 3
-    @row = 'middle' if id == 4 || id == 5 || id == 6
-    @row = 'bottom' if id == 7 || id == 8 || id == 9
-
-    @column = 'left'     if id == 1 || id == 4 || id == 7
-    @column = 'center'   if id == 2 || id == 5 || id == 8
-    @column = 'right'    if id == 3 || id == 6 || id == 9
-
-    @diagonal = 'north-east'  if id == 1 || id == 9
-    @diagonal = 'north-west'  if id == 7 || id == 3
-    @diagonal = 'special'     if id == 5
-
   end
 
 end
 
 class Board
-  attr_accessor :cells
+  attr_accessor :cells, :lines
 
   def initialize
     @cells = []
@@ -31,41 +18,39 @@ class Board
       cell = Cell.new(i)
       @cells << cell
     end
+
+    @lines = {
+      top:    @cells[0..2],
+      middle: @cells[3..5],
+      bottom: @cells[6..8]
+    }
   end
 
   def output
     @cells.each do |cell|
       print cell.state + " "
-      puts "" if cell.column == "right"
+      puts "" if cell.id % 3 == 0
     end
   end
 
+  def check_state
 
+  end
 end
 
-# Conditions!
-# def check_victor(board)
-#  @cells.each do |cell|
-#    top_x = 0
-#    if cell.state == "X" && cell.row == "top"
-#      top_x += 1
-#    end
-#  end
-#  puts top_x
-# end
-
-
-# GAME
 board = Board.new
 loop do
   board.output
+  
   puts "Player 1, choose a number of an empty cell"
   input_p1 = gets.chomp.to_i
-  board.cells[input_p1 - 1].state = "X"
-  check_victory(board)
-  board.output
-  puts "Player 1, choose a number of an empty cell"
-  input_p2 = gets.chomp.to_i
-  board.cells[input_p2 - 1].state = "O"
-  check_victory(board)
+  board.cells[input_p1 - 1].state = "x"
+  # puts board.lines[:top][0].state
+  # board.check_state
+
+  # board.output
+  # puts "Player 2, choose a number of an empty cell"
+  # input_p2 = gets.chomp.to_i
+  # board.cells[input_p2 - 1].state = "o"
+  # board.check_state
 end
